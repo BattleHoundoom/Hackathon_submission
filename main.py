38af2bd179efc2291fb77da16eced322157aaeed
emergenitro <karthik.sankar@giis.edu.sg>
2021-07-31 09:56:17 +0800

    Basic updates
    Main.py - Added in testing for environment variables
    Donsit.ico - Icon for notifications
    .env - For environment variables

--- main.py
+++ main.py
@@ -8,3 +8,9 @@ from plyer import notification
 import smtplib, ssl
 import win32gui
 import win32con
+
+from dotenv import dotenv_values
+
+config = dotenv_values(".env")
+
+print(config.get('PASSWORD'))
