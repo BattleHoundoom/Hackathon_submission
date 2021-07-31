364ec5762af01a5e58ffb13ae5940abfe17fd074
emergenitro <karthik.sankar@giis.edu.sg>
2021-07-31 17:16:13 +0800

    Update for multiple types
    Updated to fit the website and application.. Imports updated for the application

---
+++ application/main.py
@@ -0,0 +1,14 @@
+# Imports
+import psutil as psutil
+import time
+import webbrowser
+import tkinter as tk
+from tkinter import ttk
+from tkinter import * 
+from tkinter import font
+from plyer import notification
+from email.message import EmailMessage
+import mysql.connector as ms
+import smtplib, ssl
+import win32gui
+import win32con
